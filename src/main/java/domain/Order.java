9587f65c873a87b84f9b2e42919bec3bd0aebcd7
Jawaadallie <230472125@mycput.ac.za>
2026-07-19 22:40:24 +0200

    Domain update with jackson annotations

---
+++ src/main/java/domain/Order.java
@@ -0,0 +1,69 @@
+/* 
+  Order____.java
+  Author: Joshua Jonathan Bird - 230444032
+  Date: 22/03/2026
+    */
+package domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+@JsonDeserialize(builder = Order.Builder.class)
+public class Order {
+    private final String orderId;
+    private final String customerId;
+    private final String orderDate;
+    private final double totalAmount;
+
+    private Order (Builder builder) {
+        this.orderId = builder.orderId;
+        this.customerId = builder.customerId;
+        this.orderDate = builder.orderDate;
+        this.totalAmount = builder.totalAmount;
+    }
+
+    //Getters
+    public String getOrderId() {
+        return orderId;
+
+    }
+    public String getCustomerId() {
+        return customerId;
+    }
+    public String getOrderDate() {
+        return orderDate;
+    }
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    @JsonPOJOBuilder(withPrefix = "set")
+    public static class Builder {
+        private  String orderId;
+        private  String customerId;
+        private  String orderDate;
+        private  double totalAmount;
+
+        //Setters
+        public Builder setOrderId(String orderId) {
+            this.orderId = orderId;
+            return this;
+        }
+        public Builder setCustomerId(String customerId) {
+            this.customerId = customerId;
+            return this;
+        }
+        public Builder setOrderDate(String orderDate) {
+            this.orderDate = orderDate;
+            return this;
+        }
+        public Builder setTotalAmount(double totalAmount) {
+            this.totalAmount = totalAmount;
+            return this;
+        }
+
+        public Order build() {
+            return new Order(this);
+        }
+    }
+}
---
+++ src/main/java/domain/Shipment.java
@@ -0,0 +1,100 @@
+/*
+ * Shipment.java
+ * Shipment model class
+ * Author: Tlangelani Chauke
+ * Date:22march 2026
+ */
+package domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+import java.util.Date;
+
+@JsonDeserialize(builder = Shipment.Builder.class)
+public class Shipment {
+
+    private String shipmentId;
+    private String address;
+    private Date shipmentDate;
+    private Date deliveryDate;
+    private String status;
+
+    // Private constructor
+    private Shipment(Builder builder) {
+        this.shipmentId = builder.shipmentId;
+        this.address = builder.address;
+        this.shipmentDate = builder.shipmentDate;
+        this.deliveryDate = builder.deliveryDate;
+        this.status = builder.status;
+    }
+
+    // Getters
+    public String getShipmentId() {
+        return shipmentId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Date getShipmentDate() {
+        return shipmentDate;
+    }
+
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    // Builder Class
+    @JsonPOJOBuilder(withPrefix = "set")
+    public static class Builder {
+        private String shipmentId;
+        private String address;
+        private Date shipmentDate;
+        private Date deliveryDate;
+        private String status;
+
+        public Builder setShipmentId(String shipmentId) {
+            this.shipmentId = shipmentId;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder setShipmentDate(Date shipmentDate) {
+            this.shipmentDate = shipmentDate;
+            return this;
+        }
+
+        public Builder setDeliveryDate(Date deliveryDate) {
+            this.deliveryDate = deliveryDate;
+            return this;
+        }
+
+        public Builder setStatus(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder copy(Shipment shipment) {
+            this.shipmentId = shipment.shipmentId;
+            this.address = shipment.address;
+            this.shipmentDate = shipment.shipmentDate;
+            this.deliveryDate = shipment.deliveryDate;
+            this.status = shipment.status;
+            return this;
+        }
+
+        public Shipment build() {
+            return new Shipment(this);
+        }
+    }
+}
